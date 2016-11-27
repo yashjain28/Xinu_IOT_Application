6dd7df7e7240871a08a0cfcdf91db090c0ddc81e
gvp-raviteja <ganireddy.raviteja@gmail.com>
2016-11-26 19:08:34 -0500

    added led api and remote server functionality

--- app.py
+++ app.py
@@ -2,25 +2,39 @@ import socket
 from flask import Flask, request
 app = Flask(__name__)
 
-UDP_IP = "192.168.1.3"
+UDP_IP = "192.168.2.4"
 UDP_PORT = 22
-MESSAGE = "sum 123 4567"
 
+# intialize socket object
 sock = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
 sock.settimeout(5.0)
 
 @app.route("/")
 def slash():
-
+    MESSAGE = "tmpsensor read"
     resp = sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
     print resp
+    data, address = sock.recvfrom(UDP_PORT)
+    return data
+
+@app.route("/led1")
+def led1():
+    MESSAGE = ""
+    #sock.sendto(MESSAGE, (UDP_IP,UDP_PORT))
     #data, address = sock.recvfrom(UDP_PORT)
-    return "80"
+    return "OK"
+
+@app.route("/led2")
+def led2():
+    MESSAGE = ""
+    sock.sendto(MESSAGE, (UDP_IP,UDP_PORT))
+    data, address = sock.recvfrom(UDP_PORT)
+    return data
 
 @app.route("/index")
 def home():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
